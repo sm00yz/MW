8d8a9f19d68f43962655d536ffa8e48525c9bae0
SAMIA ALGHAMDI <alghamdi.smasm@gmail.com>
2025-10-06 11:40:21 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,10 @@
 import streamlit as st
-
+st.set_page_config(
+    page_title="Mental Wellness",
+    page_icon="👋",
+)
 st.title('🎈 Mental Well')
 
-st.write('your dgital mental wellness companion ')
+st.write(' your dgital mental wellness companion 🌿')
+
+st.sidebar.success("Select a demo above.")
